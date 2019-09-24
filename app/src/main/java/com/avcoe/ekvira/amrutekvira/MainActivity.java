39c59ca82e14b812ed67e9d2831778a9f7965ed4
Ekvira Foundation <ekvirafoundation@gmail.com>
2019-09-24 12:07:18 +0530

    First Commit

---
+++ app/src/main/java/com/avcoe/ekvira/amrutekvira/MainActivity.java
@@ -0,0 +1,21 @@
+package com.avcoe.ekvira.amrutekvira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.avcoe.ekvira.amrutekvira.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/com/avcoe/ekvira/amrutekvira/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package com.avcoe.ekvira.amrutekvira.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
